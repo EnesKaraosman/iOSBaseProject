6b2eb7ff37a516e2d4abf58799219e50a2505b94
EXT02D19224 <enes.karaosman@consultant.turkcell.com.tr>
2020-01-10 12:11:43 +0300

    [ADD] Configurations File added

---
+++ BaseProject/Configurations/Configurations.swift
@@ -0,0 +1,17 @@
+//
+//  Configurations.swift
+//  BaseProject
+//
+//  Created by Enes Karaosman on 10.01.2020.
+//  Copyright © 2020 Enes Karaosman. All rights reserved.
+//
+
+import UIKit
+
+struct Configurations {
+    struct Fonts {
+        static let primary: UIFont = .monospacedSystemFont(ofSize: 17, weight: .regular)
+        static let title: UIFont = .monospacedSystemFont(ofSize: 21, weight: .bold)
+        static let text: UIFont = .monospacedSystemFont(ofSize: 17, weight: .regular)
+    }
+}
