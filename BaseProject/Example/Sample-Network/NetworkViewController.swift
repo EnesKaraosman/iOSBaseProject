d508714c6b81f8f5a27d7df6ee268cb8673cdbc9
EXT02D19224 <enes.karaosman@consultant.turkcell.com.tr>
2020-01-13 16:11:28 +0300

    [MOD] Switched to BaseViewController

--- BaseProject/Example/Sample-Network/NetworkViewController.swift
+++ BaseProject/Example/Sample-Network/NetworkViewController.swift
@@ -12,7 +12,7 @@ import SnapKit
 import Toast_Swift
 import Highlightr
 
-class NetworkViewController: UIViewController, LoaderPresentable {
+class NetworkViewController: BaseViewController {
     
     private lazy var codeLabel: UILabel = {
         
@@ -38,17 +38,19 @@ class NetworkViewController: UIViewController, LoaderPresentable {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupUI()
         self.networkTest()
     }
     
-    private func setupUI() {
+    override func setupUI() {
+        super.setupUI()
+        
         self.view.backgroundColor = #colorLiteral(red: 0.1422876716, green: 0.1694026589, blue: 0.1220108792, alpha: 1)
         self.view.addSubview(codeLabel)
         self.codeLabel.snp.makeConstraints {
             $0.top.equalToSuperview().offset(100)
             $0.left.right.equalToSuperview().inset(16)
         }
+        
     }
     
     private func networkTest() {
